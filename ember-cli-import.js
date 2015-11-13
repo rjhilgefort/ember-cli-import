fc2b367231d4a365ff2ed3a1caab7fe927ea0255
Rob Hilgefort <rjhilgefort@gmail.com>
2015-11-13 12:11:03 -0500

    Project code initial commit

---
+++ ember-cli-import.js
@@ -0,0 +1,72 @@
+var _ = require('lodash');
+
+module.exports = CliImport;
+
+// ================================================================================
+// Constructor
+// ================================================================================
+
+function CliImport(app) {
+  if (_.isUndefined(app)) {
+    throw new Error('`app` must be passed to constructor');
+  }
+  this.app = app;
+};
+
+
+// ================================================================================
+// Core methods
+// ================================================================================
+
+CliImport.prototype.dep = function(dep, options) {
+  this.app.import(dep, options);
+};
+
+CliImport.prototype.depDevProd = function(dep, options) {
+  var regexp = /\.[^/.]+$/;
+  var extension = dep.match(regexp);
+
+  if (!_.isArray(extension)) {
+    throw new Error("`dep` is assumed to have an extension (and a '.min' build)");
+  }
+
+  // Grab the extension as a string if found
+  extension = _.first(extension);
+
+  // Get rid of extension now that we have it
+  dep = dep.replace(regexp, '');
+
+  this.app.import({
+    development: dep + extension,
+    production: dep + '.min' + extension
+  });
+};
+
+// ================================================================================
+// Bower shortcuts
+// ================================================================================
+
+CliImport.prototype.bower = function(dep, options) {
+  this.dep(this.app.bowerDirectory + ensureSlash(dep), options);
+};
+
+CliImport.prototype.bowerDevProd = function(dep, options) {
+  this.depDevProd(this.app.bowerDirectory + ensureSlash(dep), options);
+};
+
+CliImport.prototype.bowerFont = function(font, options) {
+  var extensions = ['eot', 'svg', 'ttf', 'woff', 'woff2'];
+  _.forEach(extensions, function(extension) {
+    this.bower(font + '.' + extension, options);
+  }, this)
+};
+
+
+// ================================================================================
+// Utility methods
+// ================================================================================
+
+function ensureSlash(dep) {
+  if (!_.startsWith(dep, '/')) dep = '/' + dep;
+  return dep;
+}
